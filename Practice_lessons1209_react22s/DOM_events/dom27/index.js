e68378e32194ca9bda4360d040a1f5c26f8cf6d3
haidanglevn <haidang.levn@gmail.com>
2022-09-12 14:53:41 +0300

    solve some problems

--- Practice_lessons1209_react22s/DOM_events/dom27/index.js
+++ Practice_lessons1209_react22s/DOM_events/dom27/index.js
@@ -1,1 +1,6 @@
 // TODO: log the button's textContent when it's clicked
+const button = document.querySelector("#my-button");
+
+button.addEventListener("click", () => {
+  console.log(button.textContent);
+});
--- Practice_lessons1209_react22s/DOM_inter/dom24/index.js
+++ Practice_lessons1209_react22s/DOM_inter/dom24/index.js
@@ -1,1 +1,5 @@
 // TODO: when the user clicks on '#menu-sidebar' => toggle the class 'show' on '#app-sidebar'
+const bar = document.querySelector("#menu-sidebar");
+bar.addEventListener("click", () => {
+  document.querySelector("#app-sidebar").classList.toggle("show");
+});
