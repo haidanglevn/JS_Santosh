d77c78fa1b0c0d38281c6f9769a567796640d5cf
haidanglevn <haidang.levn@gmail.com>
2022-09-26 15:02:16 +0300

    parcelBundle folder

---
+++ parcelBundle/src/app.js
@@ -0,0 +1,8 @@
+import name from "./multipleFiles";
+import simple_number_formatter from "@skalwar/simple_number_formatter";
+
+console.log("Hello");
+console.log("Hello ", name);
+
+let result = simple_number_formatter(95566);
+console.log(result);
