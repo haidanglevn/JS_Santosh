f117a347428b865f3a9f12c3a09be8a82f9d13d3
haidanglevn <haidang.levn@gmail.com>
2022-09-26 13:35:06 +0300

    new files

--- Practice_lessons2609_react22s/fetch/fetch4/fetch4.js
+++ Practice_lessons2609_react22s/fetch/fetch4/fetch4.js
@@ -1,13 +1,14 @@
-"use strict";
+'use strict';
 
 class FetchWrapper {
-  constructor(baseURL) {
-    this.baseURL = baseURL;
-  }
+    constructor(baseURL) {
+        this.baseURL = baseURL;
+    }
 
-  get(endpoint) {
-    return fetch(this.baseURL + endpoint).then((response) => response.json());
-  }
+    get(endpoint) {
+        return fetch(this.baseURL + endpoint)
+            .then(response => response.json());
+    }
 }
 
 /*
@@ -15,17 +16,15 @@ Complete the checkForNewNotifications function such that it makes a fetch re
 and return its result. Also, visualize that the result of fetch is a Promise. You should see Promise in the console.
 */
 
+
 const getChapters = () => {
-  // TODO
-  fetch(
-    "https://programmingjs-90a13-default-rtdb.europe-west1.firebasedatabase.app/chapters.json"
-  );
-  get(chapters);
-};
+    // TODO
+
+}
 
 // do NOT modify this function
 function displayCompletedChapters(chapters) {
-  console.log("Received", chapters);
+    console.log("Received", chapters);
 }
 
 // Sample usage - do not modify
