60d1a440d55c1723817d45c1ef4f39a837e7faab
haidanglevn <haidang.levn@gmail.com>
2022-09-26 14:07:43 +0300

    done some async-await practices

--- Practice_lessons2609_react22s/async_await/async_await03.js
+++ Practice_lessons2609_react22s/async_await/async_await03.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
 Rewrite/refactor the canVote function as an async arrow function.
@@ -7,16 +7,18 @@ Rewrite/refactor the canVote function as an async arrow function.
 /**
  * @param {number} age
  */
-const canVote = age => {
-    return new Promise(resolve => {
-        resolve(age >= 18);
-    });
-}
-
+/* const canVote = (age) => {
+  return new Promise((resolve) => {
+    resolve(age >= 18);
+  });
+}; */
+const canVote = async (age) => {
+  return age >= 18;
+};
 // Sample usage - do not modify
-canVote(20).then(result => {
-    console.log(result); // true
+canVote(20).then((result) => {
+  console.log(result); // true
 });
-canVote(15).then(result => {
-    console.log(result); // false
+canVote(15).then((result) => {
+  console.log(result); // false
 });
--- Practice_lessons2609_react22s/async_await/async_await07/async_await07.js
+++ Practice_lessons2609_react22s/async_await/async_await07/async_await07.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
 Complete the getTweetDetails function such that it fetches the following endpoint
@@ -7,14 +7,22 @@ and then calls the showAuthorName function with the full name of tweet author
 (the full name should contain a space character  between the first name and the last name).
 You should use async/await */
 
-const getTweetDetails = () => {
-    // TODO using async/await
-
-}
+const getTweetDetails = async () => {
+  // TODO using async/await
+  const response = await fetch(
+    "https://programmingjs-90a13-default-rtdb.europe-west1.firebasedatabase.app/tweet.json"
+  );
+  const data = await response.json();
+  console.log(data);
+  let fullName = `${
+    data.author.details.firstName + " " + data.author.details.lastName
+  }`;
+  showAuthorName(fullName);
+};
 
 // do NOT modify this function
 function showAuthorName(fullName) {
-    console.log(fullName);
+  console.log(fullName);
 }
 
 // Sample usage - do not modify
